86d86c88705295b149b4cd11eed4821304ac1cbd
Kas Galstaun <kgalstaun@fastmail.com>
2023-03-17 10:12:08 +0900

    Remove transform to lowercase for typewriter

--- src/helpers/TypewriterHelper.js
+++ src/helpers/TypewriterHelper.js
@@ -3,7 +3,7 @@ import TimeOutHelper from "@/helpers/TimeOutHelper.js";
 const Typewriter = {
   async startAnimation(animatedText, originalText, speed, index) {
     let text = originalText;
-    let textArray = text.replace(/ /g, " ").toLowerCase().split("");
+    let textArray = text.replace(/ /g, " ").split("");
 
     for (const character of textArray) {
       await TimeOutHelper.startTimeout(speed);
